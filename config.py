bd6dc60b1d04ddfa048a8f3a45a1edec379fc1c2
Michael <137332425+mmmichael777@users.noreply.github.com>
2026-08-13 17:18:06 -0700

    Add team data and retrieval function in config.py
    Added NBA team data and a function to retrieve team information by abbreviation.

--- config.py
+++ config.py
@@ -1,1 +1,26 @@
+from nba_api.stats.static import teams
 
+def get_team(team_abbreviation):
+    return teams.find_team_by_abbreviation(team_abbreviation)
+
+
+team = get_team("NYK")
+
+print(team)
+
+TEAM_NAME = "New York Knicks"
+TEAM_ID = "1610612752"
+CONFERENCE = "East"
+
+TEAMS = {
+  "Knicks": {
+    "full name": "New York Knicks", 
+    "id": 1610612752,
+    "abbreviation": "NYK",
+    "nickname": "Knicks",
+    "Conference": "East",
+  }, 
+
+  "Celtics": {
+    "name": "Boston Celtics", 
+    "id": 
